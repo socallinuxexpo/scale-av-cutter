69606a15023ca2c0672a3a28a9498005d0fa06cc
Vincent Wong <duperduper@ucla.edu>
2020-03-01 01:22:49 -0800

    config: get SECRET_KEY from environ

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = os.environ['SECRET_KEY']
     ADMIN_KEY = os.environ['ADMIN_KEY']
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
