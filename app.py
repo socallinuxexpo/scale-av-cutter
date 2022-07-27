2ab17213f3a330a95f564c24f6f078024d933de8
Vincent Wong <duperduper@ucla.edu>
2022-07-27 00:06:13 -0700

    db: migration setup

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
