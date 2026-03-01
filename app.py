561874908385ce3486789c731113ed3e604bdcd6
Jess.Bermudes <jbermudes@gmail.com>
2026-02-28 20:46:01 -0800

    Add import schedule button

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+from time import time
 import os
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.jinja_env.globals['static_ts'] = int(time())
 
 db = SQLAlchemy(app)
 
